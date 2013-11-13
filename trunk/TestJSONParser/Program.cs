db7e67a21251ed4544205f16d00f59ea5c4ca01d
ysharp.design@gmail.com <ysharp.design@gmail.com@e47a73c6-b22a-ef22-9566-2b901a3d25de>
2013-11-13 09:21:28 +0000

    Small refacto

--- trunk/TestJSONParser/Program.cs
+++ trunk/TestJSONParser/Program.cs
@@ -1,63 +1,74 @@
-﻿using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Web.Script.Serialization;
 
 namespace TestJSONParser
 {
-	class Program
-	{
-		// Note: fathers.json.txt was generated using:
-		// http://experiments.mennovanslooten.nl/2010/mockjson/tryit.html
-		private const string TEST_FILE_PATH = @"..\..\fathers.json.txt";
+    class Program
+    {
+        // Note: fathers.json.txt was generated using:
+        // http://experiments.mennovanslooten.nl/2010/mockjson/tryit.html
+        private const string FATHERS_TEST_FILE_PATH = @"..\..\fathers.json.txt";
+        private const string SMALL_TEST_FILE_PATH = @"..\..\small.json.txt";
 
-		static void Main(string[] args)
-		{
-			string json = System.IO.File.ReadAllText(TEST_FILE_PATH);
-			Console.WriteLine("JSON parse... {0} kb ({1} mb)", (int)(json.Length / 1024), (int)(json.Length / (1024 * 1024)));
-			Console.WriteLine();
+        static void Main(string[] args)
+        {
+            string small = System.IO.File.ReadAllText(SMALL_TEST_FILE_PATH);
+            Console.WriteLine("Small Test - JSON parse... {0} bytes ({1} kb)", small.Length, ((decimal)small.Length / (decimal)1024));
+            Console.WriteLine();
 
-			JavaScriptSerializer serializer = new JavaScriptSerializer
-			{
-				MaxJsonLength = int.MaxValue
-			};
-			Console.WriteLine("Parsed by {0} in...", serializer.GetType().FullName);
-			DateTime st = DateTime.Now;
-			var msObj = serializer.Deserialize<object>(json);
-			Console.WriteLine("... {0} ms", (int)DateTime.Now.Subtract(st).TotalMilliseconds);
-			Console.WriteLine();
+            var parser = new JSONParser();
+            Console.WriteLine("\tParsed by {0} in...", parser.GetType().FullName);
+            DateTime start = DateTime.Now;
+            object obj = parser.Parse(System.IO.File.ReadAllText(SMALL_TEST_FILE_PATH));
+            Console.WriteLine("\t\t{0} ms", (int)DateTime.Now.Subtract(start).TotalMilliseconds);
+            Console.WriteLine();
 
-			Console.WriteLine("Parsed by {0} in...", typeof(System.Text.JSONParser).FullName);
-			DateTime start = DateTime.Now;
-			object myObj = System.Text.JSONParser.Parse(json);
-			Console.WriteLine();
-			Console.WriteLine("... {0} ms", (int)DateTime.Now.Subtract(start).TotalMilliseconds);
-			Console.WriteLine();
+            string json = System.IO.File.ReadAllText(FATHERS_TEST_FILE_PATH);
+            Console.WriteLine("Fathers Test - JSON parse... {0} kb ({1} mb)", (int)(json.Length / 1024), (int)(json.Length / (1024 * 1024)));
+            Console.WriteLine();
 
-			Console.WriteLine("Press '1' to inspect our result object,\r\nany other key to inspect Microsoft's JS serializer result object...");
-			// Works also: var parsed = msObj; for the object returned by Microsoft's JS serializer
-			var parsed = ((Console.ReadKey().KeyChar == '1') ? myObj : msObj);
+            var serializer = new System.Web.Script.Serialization.JavaScriptSerializer
+            {
+               MaxJsonLength = int.MaxValue
+            };
+            Console.WriteLine("\tParsed by {0} in...", serializer.GetType().FullName);
+            DateTime start1 = DateTime.Now;
+            var msObj = serializer.Deserialize<object>(json);
+            Console.WriteLine("\t\t{0} ms", (int)DateTime.Now.Subtract(start1).TotalMilliseconds);
+            Console.WriteLine();
 
-			object[] items = (object[])((Dictionary<string, object>)parsed)["fathers"];
-			Console.WriteLine();
-			Console.WriteLine("Found : {0} fathers", items.Length);
-			Console.WriteLine("Press a key to list them...");
-			Console.WriteLine();
-			Console.ReadKey();
-			foreach (object item in items)
-			{
-				var father = (Dictionary<string, object>)item;
-				var name = (string)father["name"];
-				var sons = (object[])father["sons"];
-				var daughters = (object[])father["daughters"];
-				Console.WriteLine("{0} : {1} son(s), and {2} daughter(s)", name, sons.Length, daughters.Length);
-			}
-			Console.WriteLine();
-			Console.WriteLine("The end. Press a key...");
+            var jsonParser = new JSONParser();
+            Console.WriteLine("\tParsed by {0} in...", jsonParser.GetType().FullName);
+            DateTime start2 = DateTime.Now;
+            object myObj = jsonParser.Parse(json);
+            Console.WriteLine("\t\t{0} ms", (int)DateTime.Now.Subtract(start2).TotalMilliseconds);
+            Console.WriteLine();
 
-			Console.ReadKey();
-		}
-	}
+            Console.WriteLine("Press '1' to inspect our result object,\r\nany other key to inspect Microsoft's JS serializer result object...");
+            // Works also: var parsed = msObj; for the object returned by Microsoft's JS serializer
+            var parsed = ((Console.ReadKey().KeyChar == '1') ? myObj : msObj);
+
+            IList<object> items = (IList<object>)((IDictionary<string, object>)parsed)["fathers"];
+            Console.WriteLine();
+            Console.WriteLine("Found : {0} fathers", items.Count);
+            Console.WriteLine();
+            Console.WriteLine("Press a key to list them...");
+            Console.WriteLine();
+            Console.ReadKey();
+            foreach (object item in items)
+            {
+                var father = (IDictionary<string, object>)item;
+                var name = (string)father["name"];
+                var sons = (IList<object>)father["sons"];
+                var daughters = (IList<object>)father["daughters"];
+                Console.WriteLine("{0} : {1} son(s), and {2} daughter(s)", name, sons.Count, daughters.Count);
+            }
+            Console.WriteLine();
+            Console.WriteLine("The end. Press a key...");
+
+            Console.ReadKey();
+        }
+    }
 }
