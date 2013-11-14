0c76af23cf5fdc49d22583412a83d2cd465990a7
ysharp.design@gmail.com <ysharp.design@gmail.com@e47a73c6-b22a-ef22-9566-2b901a3d25de>
2013-11-14 01:59:36 +0000

    Added online example using System.IO.Stream support

--- trunk/TestJSONParser/Program.cs
+++ trunk/TestJSONParser/Program.cs
@@ -14,8 +14,46 @@ namespace TestJSONParser
 		private const string FATHERS_TEST_FILE_PATH = @"..\..\fathers.json.txt";
 		private const string SMALL_TEST_FILE_PATH = @"..\..\small.json.txt";
 
+		static void Top10Youtube2013Test()
+		{
+			Console.WriteLine("Top 10 Youtube 2013 Test - JSON parse...");
+			Console.WriteLine();
+			System.Net.WebRequest www = System.Net.WebRequest.Create("https://gdata.youtube.com/feeds/api/videos?q=2013&max-results=10&v=2&alt=jsonc");
+			using (System.IO.Stream stream = www.GetResponse().GetResponseStream())
+			{
+				var parser = new JSONParser();
+				Console.WriteLine("\tParsed by {0} in...", parser.GetType().FullName);
+				DateTime start = DateTime.Now;
+				object parsed = parser.Parse(stream);
+				Console.WriteLine("\t\t{0} ms", (int)DateTime.Now.Subtract(start).TotalMilliseconds);
+				Console.WriteLine();
+
+				var data = (IDictionary<string, object>)((IDictionary<string, object>)parsed)["data"];
+				var items = (IList<object>)((IDictionary<string, object>)data)["items"];
+				Console.WriteLine("Press a key...");
+				Console.WriteLine();
+				Console.ReadKey();
+				foreach (object item in items)
+				{
+					var post = (IDictionary<string, object>)item;
+					var player = (IDictionary<string, object>)post["player"];
+					var title = (string)post["title"];
+					var category = (string)post["category"];
+					var uploaded = (string)post["uploaded"];
+					var link = (string)player["default"];
+					Console.WriteLine("\t\"{0}\" (category: {1}, uploaded: {2})", title, category, uploaded);
+					Console.WriteLine("\t\tURL: {0}", link);
+					Console.WriteLine();
+				}
+				Console.WriteLine("Press a key...");
+				Console.WriteLine();
+			}
+		}
+
 		static void Main(string[] args)
 		{
+			Top10Youtube2013Test();
+
 			string small = System.IO.File.ReadAllText(SMALL_TEST_FILE_PATH);
 			Console.WriteLine("Small Test - JSON parse... {0} bytes ({1} kb)", small.Length, ((decimal)small.Length / (decimal)1024));
 			Console.WriteLine();
