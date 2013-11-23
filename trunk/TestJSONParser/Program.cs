0534d10cfe618703a752a720622bb66050b2967e
ysharp.design@gmail.com <ysharp.design@gmail.com@e47a73c6-b22a-ef22-9566-2b901a3d25de>
2013-11-23 08:35:30 +0000

    Hmm...

--- trunk/TestJSONParser/Program.cs
+++ trunk/TestJSONParser/Program.cs
@@ -3,12 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Text.Json;
+
 namespace TestJSONParser
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			/*Hmm...
+            Func<int> foo = () => 123;
+            Func<object> up1 = JSON.UpCast(() => foo());
+            object n = JSON.Call(typeof(int), up1);*/
+
 			BasicTests.MostBasicTest();
 			BasicTests.DateTimeTest();
 			BasicTests.PersonTest();
