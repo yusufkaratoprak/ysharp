d2c2ab28ed0fe48b05e39f0aafc9fd53cd6c6211
ysharp.design@gmail.com <ysharp.design@gmail.com@e47a73c6-b22a-ef22-9566-2b901a3d25de>
2014-05-10 08:50:22 +0000

    Simpler interface

--- StateMachinesPoC/Program.cs
+++ StateMachinesPoC/Program.cs
@@ -1,18 +1,41 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 
 namespace Test
 {
-	class Program
-	{
-		static void Main(string[] args)
-		{
-			ThatsLife.Example.Run();
-			WatchingTV.Example.Run();
-			WatchingTVAdvanced.Example.Run();
-		}
-	}
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                char choice;
+                Console.Clear();
+                Console.WriteLine("Press \"1\" for \"That's Life\" sample");
+                Console.WriteLine("Press \"2\" for \"Watching TV\" sample");
+                Console.WriteLine("Press \"3\" for \"Watching TV Advanced\" sample");
+                Console.WriteLine();
+                Console.WriteLine("Press ESC to exit");
+                if ((choice = Console.ReadKey().KeyChar) != 27)
+                    switch (choice)
+                    {
+                        case '1':
+                            ThatsLifeSample.Run();
+                            break;
+                        case '2':
+                            WatchingTvSample.Run();
+                            break;
+                        case '3':
+                            WatchingTvSampleAdvanced.Run();
+                            break;
+                        default:
+                            break;
+                    }
+                else
+                    break;
+            }
+        }
+    }
 }
